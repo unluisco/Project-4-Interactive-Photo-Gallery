c6b6d17e453c62b2ab6fb23dbe34b3bb5ba95e19
Luis Romero <diceluisco@gmail.com>
2018-01-24 23:58:17 -0500

    add search functionality

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-$('#lightbox').Chocolat({ enableZoom: false });
+$( '#lightbox' ).Chocolat( { enableZoom: false } );
+
+// Search functionality
+$( '.inputSearch' ).on( 'keyup', function() {
+	var filter = $( this ).val().toLowerCase();
+
+	$( '.chocolat-image' ).each( function() {
+
+		if ( $( this ).attr( 'title' ).toLowerCase().search( new RegExp( filter ) ) < 0 ) {
+			$( this ).fadeOut();
+		} else {
+			$( this ).show();
+		}
+
+	});
+});
